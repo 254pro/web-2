2d623e214df201612b6365d227196c32819da813
WebGeek <117008033+254pro@users.noreply.github.com>
2024-03-14 09:55:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("i like pitza");
+let Age;
+Age=21;
+console.log(Age);
+let firstname="collins";
+let student=false;
+console.log(Age);
+console.log(student);
+console.log(firstname);
+console.log("bro",firstname,"perhaps your age is ",Age, "enrolled:",student);
+document.getElementById("p1").innerHTML = "hello" + firstname;s
